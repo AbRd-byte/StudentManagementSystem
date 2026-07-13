57e8d70b21f05648da1ad6822f9d1e78aaf7705e
Abhishek Singh <an.abhisinghmoto@gmail.com>
2026-07-13 14:58:38 +0530

    Modified Student controller to added try catch

--- StudentManagementSystem/Controllers/StudentsController.cs
+++ StudentManagementSystem/Controllers/StudentsController.cs
@@ -21,51 +21,87 @@ namespace StudentManagementSystem.Controllers
         [HttpGet("all")]
         public async Task<IActionResult> GetAllStudents()
         {
-            var students = await _studentService.GetAllStudentsAsync();
+            try
+            {
+                var students = await _studentService.GetAllStudentsAsync();
 
-            return Ok(students);
+                return Ok(students);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpGet("{id:int}/student")]
         public async Task<IActionResult> GetStudentById(int id)
         {
-            var student = await _studentService.GetStudentByIdAsync(id);
-            if (student == null)
+            try
             {
-                return NotFound($"Student with ID {id} not found.");
+                var student = await _studentService.GetStudentByIdAsync(id);
+                if (student == null)
+                {
+                    return NotFound($"Student with ID {id} not found.");
+                }
+                return Ok(student);
             }
-            return Ok(student);
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPost("insertrecords")]
         public async Task<IActionResult> InsertStudent([FromBody] CreateStudentDTO studentDto)
         {
-            if (studentDto == null)
+            try
             {
-                return BadRequest("Student data is null.");
+                if (studentDto == null)
+                {
+                    return BadRequest("Student data is null.");
+                }
+                var createdStudent = await _studentService.CreateStudentAsync(studentDto);
+                return CreatedAtAction(nameof(GetAllStudents), new { id = createdStudent.Id }, createdStudent);
             }
-            var createdStudent = await _studentService.CreateStudentAsync(studentDto);
-            return CreatedAtAction(nameof(GetAllStudents), new { id = createdStudent.Id }, createdStudent);
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPost("{id:int}/updaterecords")]
         public async Task<IActionResult> UpdateStudent([FromBody] UpdateStudentDTO studentDto, int id)
         {
-            if (studentDto == null)
+            try
             {
-                return BadRequest("Student data is null.");
+                if (studentDto == null)
+                {
+                    return BadRequest("Student data is null.");
+                }
+                var updatedStudent = await _studentService.UpdateStudentAsync(id, studentDto);
+                if (updatedStudent == null)
+                {
+                    return NotFound($"Student with ID {studentDto.Id} not found.");
+                }
+                return Ok(updatedStudent);
             }
-            var updatedStudent = await _studentService.UpdateStudentAsync(id, studentDto);
-            if (updatedStudent == null)
+            catch (Exception ex)
             {
-                return NotFound($"Student with ID {studentDto.Id} not found.");
-            }
-            return Ok(updatedStudent);
+                return StatusCode(500, ex.Message);
+            }            
         }
 
         [HttpDelete("deleterecords/{id}")]
         public async Task<IActionResult> DeleteStudent(int id)
         {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
             var isDeleted = await _studentService.DeleteStudentAsync(id);
             if (!isDeleted)
             {
