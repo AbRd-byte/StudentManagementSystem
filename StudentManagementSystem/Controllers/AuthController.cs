e47537e80efa114819c9b3949752cd1e56d5940a
Abhishek Singh <an.abhisinghmoto@gmail.com>
2026-07-14 10:08:28 +0530

    Add AuthController, for the authentication functionality implementation.

---
+++ StudentManagementSystem/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("AuthController is working!");
+        }
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("AuthController POST is working!");
+        }
+    }
+}
